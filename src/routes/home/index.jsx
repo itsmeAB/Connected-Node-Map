0c6e6c0b3433b3d0b8ff84128cc61b8def74d398
itsmeAB <ss.abhilash3@gmail.com>
2020-07-15 18:30:02 +0530

    add react-d3-graph

--- src/routes/home/index.jsx
+++ src/routes/home/index.jsx
@@ -3,12 +3,35 @@ import NodeMap from '../../components/NodeMap';
 import data from '../../utils/data.json';
 
 const Home = (props) => {
-    console.log(data);
+  //   console.log(data);
+  // const members = data.map(item => item.members);
+
+  const members = data.reduce((acc, cur) => {
+    const mbs = cur.members.map((mb) => ({ ...mb, id: mb.din,  }));
+    acc = [...acc, ...mbs];
+    return acc;
+  }, []);
+
+  const organizations = data.map((item) => ({
+    ...item.organization,
+    id: item.organization.cin,
+  }));
+
+  let links = [];
+  data.forEach((item) => {
+    const members = item.members;
+    const link = members.map((mb) => ({
+      source: mb.din,
+      target: item.organization.cin,
+    }));
+    links = [...links, ...link];
+  });
+  console.log(members, organizations);
   return (
     <div>
       HOME ROUTE
       <div>
-        <NodeMap />
+        <NodeMap nodes={[...members, ...organizations]} links={links} />
       </div>
     </div>
   );
